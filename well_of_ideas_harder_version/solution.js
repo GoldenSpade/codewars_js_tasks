d391078575d0be9c741ba0a69f900de6036261d5
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-06-05 16:16:59 +0300

    The task «Well of Ideas - Harder Version» was done

---
+++ well_of_ideas_harder_version/solution.js
@@ -0,0 +1,37 @@
+// input data
+
+const arr = [
+  ['GoD', 'bad', 'bAd', 'bad'],
+  ['bad', 'bAd', 'bad'],
+  ['bad', 'bAd', 'bad', 8, 9, 10, 15, 29]
+];
+
+// solution
+
+function well(x) {
+  let count = 0;
+
+  const findAll = x.map((a) => {
+    return a.map((b) => {
+      if (String(b).toLowerCase() === 'good') {
+        count++;
+      }
+    })
+  })
+
+  if (count > 0 && count <= 2) {
+    return 'Publish!';
+  }
+
+  if (count > 2) {
+    return 'I smell a series!';
+  }
+
+  if (count === 0) {
+    return 'Fail!';
+  }
+}
+
+//control
+
+console.log(well(arr));
