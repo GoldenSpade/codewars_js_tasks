68eb79fd9a8afac7989d661ec4bd30593ebdb779
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-06-15 22:37:56 +0300

    The task «Binary Addition» was completed

---
+++ js_task_binary_addition/solution.js
@@ -0,0 +1,14 @@
+// input data
+
+const num1 = 5;
+const num2 = 9;
+
+// solution
+
+function addBinary(a, b) {
+  return (a + b).toString(2);
+}
+
+// control
+
+console.log(addBinary(num1, num2));
