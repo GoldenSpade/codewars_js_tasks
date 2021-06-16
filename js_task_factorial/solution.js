b2acece16b8335cf6714660bff04a9651c24a8d9
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-06-17 00:03:13 +0300

    update of solution

--- js_task_factorial/solution.js
+++ js_task_factorial/solution.js
@@ -5,16 +5,10 @@ const number = 5;
 // solution
 
 function factorial(n) {
-  const arrOfNums = [];
-
-  for (let i = 1; i <= n; i++) {
-    arrOfNums.push(i);
-  }
-
   let result = 1;
 
-  for (let i = 0; i < arrOfNums.length; i++) { // or we can return arrOfNums.reduce((a, b) => a * b);
-    result *= arrOfNums[i];
+  for (let i = 1; i <= n; i++) {
+    result *= i;
   }
 
   return result;
