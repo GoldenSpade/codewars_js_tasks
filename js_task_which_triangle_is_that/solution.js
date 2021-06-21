ad2e5c926996ad0bd76ab1fefa36cf48cfb46bd5
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-06-21 22:36:42 +0300

    The task «Which triangle is that?» was completed

---
+++ js_task_which_triangle_is_that/solution.js
@@ -0,0 +1,19 @@
+// solution
+
+const typeOfTriangle = function (a, b, c) {
+  if ([a, b, c].some(x => typeof x != "number")) return "Not a valid triangle";
+  
+  [a, b, c] = [a, b, c].sort((x, y) => x-y);
+  
+  if (a + b <= c) return "Not a valid triangle";
+  
+  if (a == b && b == c) return "Equilateral";
+  
+  if (a == b || a == c || b == c) return "Isosceles";
+  
+  return "Scalene";
+}
+
+// control
+
+console.log(typeOfTriangle(12, 12, 11));
