1ca7ce92eb80ebc3dca1c3e9634df5e91a8e7f05
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-06-21 22:51:36 +0300

    The task «Average Scores» was completed

---
+++ js_task_average_scores/solution.js
@@ -0,0 +1,16 @@
+// input data
+
+const scores = [90, 98, 89, 100, 100, 86, 94];
+
+// solution
+
+function average(scores) {
+  let sum = scores.reduce((a, b) => a + b);
+  
+  return Math.round(sum / scores.length)
+}
+
+
+// control
+
+console.log(average(scores));
