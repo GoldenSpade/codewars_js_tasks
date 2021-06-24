ceb6d78f6d9588be318877d04f7c103e13c15668
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-06-24 22:57:43 +0300

    The task «Thinkful - String Drills: Repeater» was done

--- js_task_thinkful_string_drills_repeater/solution.js
+++ js_task_thinkful_string_drills_repeater/solution.js
@@ -1,8 +1,14 @@
+// input data
+
 const str = 'a';
 const number = 5;
+
+// solution
 
 function repeater(string, n) {
   return string.repeat(n);
 }
+
+// control
 
 console.log(repeater(str, number));
