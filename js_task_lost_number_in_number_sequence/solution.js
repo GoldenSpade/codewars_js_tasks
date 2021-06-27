7973e5a81d05c7bf95a9d10e295237895288500b
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-06-27 23:54:41 +0300

    The task «Lost number in number sequence» was completed

---
+++ js_task_lost_number_in_number_sequence/solution.js
@@ -0,0 +1,18 @@
+// input data
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const mixArr = [1, 9, 7, 4, 6, 2, 3, 8];
+
+// solution
+
+function findDeletedNumber(arr, mixArr) {
+  if(arr.length === mixArr.length) return 0;
+  for(var k in arr)
+    if(mixArr.indexOf(arr[k]) === -1)
+      return arr[k];
+  return 0;
+}
+
+// control
+
+console.log(findDeletedNumber(arr, mixArr));
