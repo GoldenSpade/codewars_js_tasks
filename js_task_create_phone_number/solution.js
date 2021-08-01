c9dc442b5be428baf42d97f560192faef89b6810
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-08-01 22:13:26 +0300

    The task «Create Phone Number» was completed

---
+++ js_task_create_phone_number/solution.js
@@ -0,0 +1,13 @@
+// input data
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+// solution
+
+function createPhoneNumber(numbers) {
+  return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+}
+
+// control
+
+console.log(createPhoneNumber(arr));
