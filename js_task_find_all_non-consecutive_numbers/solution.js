4cf97fd2ff29406d432c94e9c295007b3118164b
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-08-20 22:38:58 +0300

    The task «Find all non-consecutive numbers» was completed

---
+++ js_task_find_all_non-consecutive_numbers/solution.js
@@ -0,0 +1,18 @@
+const arr = [1, 2, 3, 4, 6, 7, 8, 10];
+
+function allNonConsecutive (array) {
+  const result = [];
+  array.push(10);
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i + 1] - array[i] > 1) {
+      result.push({
+        i: i + 1,
+        n: array[i + 1]
+      })
+    }
+  };
+  return result;
+}
+
+console.log(allNonConsecutive(arr));
