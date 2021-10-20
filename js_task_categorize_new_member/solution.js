906c61465d431a5bdd996748d1a5cff14d2bc3c9
Vyacheslav Sergienko <vjacheslav.sergienko.01@gmail.com>
2021-10-20 18:52:58 +0300

    The task «Categorize New Member» was completed

---
+++ js_task_categorize_new_member/solution.js
@@ -0,0 +1,14 @@
+const array = [
+  [18, 20],
+  [45, 2],
+  [61, 12],
+  [37, 6],
+  [21, 21],
+  [78, 9]
+];
+
+function openOrSenior(data) {
+  return data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open');
+}
+
+console.log(openOrSenior(array));
